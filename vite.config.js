cb8d7c9954e15a40bfdec96106b7556505cf51cc
Anahi Gimenez <anahi.gimenez@fiuni.edu.py>
2026-09-13 23:20:09 -0300

    Fix: configurar ruta base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+  plugins: [react()],
+  base: '/Mini-erp-app_AnahiGimenez/', 
 })
